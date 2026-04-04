21369cc201bc3652056d672dfac3d148bd7a5045
Pēteris Krūmiņš <peteris.krumins@lightsoft.lv>
2026-04-04 15:12:44 +0300

    Added swagger and user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Octopus.Api.Controllers
+{
+    [Route("api")]
+    public class UserController : ControllerBase
+    {
+        public async Task<IEnumerable<int>> GetUserIds() {
+            return [];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
