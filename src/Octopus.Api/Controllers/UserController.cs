39e7d09b1031ffa7cbe8b624b018f6e214f5063f
Pēteris Krūmiņš <peteris.krumins@lightsoft.lv>
2026-04-04 17:16:46 +0300

    Sorted folder structure, added stricter rules

--- src/Octopus.Api/Controllers/UserController.cs
+++ src/Octopus.Api/Controllers/UserController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Octopus.Core.User;
 
 namespace Octopus.Api.Controllers
 {
     [Route("api")]
     public class UserController : ControllerBase
     {
-        public async Task<IEnumerable<int>> GetUserIds() {
+        public static async Task<IEnumerable<User>> GetUsersAsync()
+        {
             return [];
         }
     }
---
+++ src/Octopus.Core/User/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Octopus.Core.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+    }
+}
