2da90a5d3039d9cc7df292527474212a622783a1
Pēteris Krūmiņš <55082100+Astonkajis@users.noreply.github.com>
2026-04-04 17:55:35 +0300

    Added base api controller, base Entity, mandatory public element summaries. (#3)
    Co-authored-by: Pēteris Krūmiņš <peteris.krumins@lightsoft.lv>

--- src/Octopus.Api/Program.cs
+++ src/Octopus.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
---
+++ src/Octopus.Core/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace Octopus.Core
+{
+    /// <summary>
+    /// Represents a base class for entities with a unique identifier.
+    /// </summary>
+    public abstract class Entity
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the entity.
+        /// </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
