82502f58eb47b6845e6dafc1b7a4db65394c81a4
Pēteris Krūmiņš <peteris.krumins@lightsoft.lv>
2026-04-04 18:10:58 +0300

    Added ulid

--- src/Octopus.Core/Entity.cs
+++ src/Octopus.Core/Entity.cs
@@ -8,6 +8,6 @@
         /// <summary>
         /// Gets or sets the unique identifier for the entity.
         /// </summary>
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Ulid Id { get; set; } = Ulid.NewUlid();
     }
 }
